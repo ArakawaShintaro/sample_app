7c1d985eac33760efa077b0125e38c4fae36b311
Arakawa Shintaro <arashinbe@gmail.com>
2018-05-28 16:45:11 +0900

    Impelent basic login

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,21 @@
+module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    # ||=演算子を使うことでメモ化
+    # なんどもクエリを発行しなくてよくなる
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def log_out
+    binding.pry
+    session.delete(:user_id)
+    @current_user = nil
+  end
+end
